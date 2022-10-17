977149e74a09e99e30c943d2dd068a78908e6578
James Seung Hyun Son <jamessoun93@gmail.com>
2022-10-17 23:50:16 +0900

    item

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,4 +27,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    // 비즈니스 로직
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int stock = this.stockQuantity - quantity;
+        if (stock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = stock;
+    }
 }
